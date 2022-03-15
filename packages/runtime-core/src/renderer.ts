beba9cdedc14632932b064229d6acc11609269a7
Roading <sevensoft@yeah.net>
2022-03-15 23:19:29 +0800

    docs: props read

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1296,6 +1296,7 @@ function baseCreateRenderer(
 
   const updateComponent = (n1: VNode, n2: VNode, optimized: boolean) => {
     const instance = (n2.component = n1.component)!
+    // 根据新旧子组件 vnode 判断是否需要更新子组件
     if (shouldUpdateComponent(n1, n2, optimized)) {
       if (
         __FEATURE_SUSPENSE__ &&
@@ -1313,16 +1314,17 @@ function baseCreateRenderer(
         }
         return
       } else {
-        // normal update
+        // normal update  新的子组件vnode赋值给instance.next
         instance.next = n2
         // in case the child component is also queued, remove it to avoid
         // double updating the same child component in the same flush.
+        //  子组件也可能因为数据变化被添加到更新队列里了，移除它们防止对一个子组件重复更新
         invalidateJob(instance.update)
-        // instance.update is the reactive effect.
+        // instance.update is the reactive effect. 执行子组件的副作用渲染函数
         instance.update()
       }
     } else {
-      // no update needed. just copy over properties
+      // no update needed. just copy over properties 执行子组件的副作用渲染函数
       n2.component = n1.component
       n2.el = n1.el
       instance.vnode = n2
